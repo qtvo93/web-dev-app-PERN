a9d9bb2ce503bda8d82b814c1a87b5aa8374dc26
Quoc Thinh Vo <72519491+qtvo93@users.noreply.github.com>
2022-03-16 00:14:39 -0400

    updated validations, changed database query, and reconstructed components

--- back-end/validations/authorize.js
+++ back-end/validations/authorize.js
@@ -1,25 +1,18 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-//this middleware will on continue on if the token is inside the local storage
-
 module.exports = async function(req, res, next) {
-  // Get token from header
   const token = req.header("jwt_token");
 
-  // Check if not token
   if (!token) {
-    return res.status(403).json({ msg: "authorization denied" });
+    return res.status(403).json({ server_message: "authorization denied" });
   }
 
-  // Verify token
   try {
-    //it is going to give use the user id (user:{id: user.id})
     const verify = jwt.verify(token, process.env.jwtSecret);
-
     req.user = verify.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
+    res.status(401).json({ server_message: "Token is not valid" });
   }
 };
--- back-end/validations/serverValidation.js
+++ back-end/validations/serverValidation.js
@@ -1,22 +1,30 @@
 module.exports = function(req, res, next) {
     const { email, name, password } = req.body;
   
-    function validEmail(userEmail) {
-      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+    function validEmail(user_Email) {
+      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user_Email);
     }
-  
+
+    function validPass(user_Password) {
+      return user_Password.length > 4;
+    }
+   
     if (req.path === "/register") {
       //console.log(!email.length);
       if (![email, name, password].every(Boolean)) {
         return res.json("Missing Credentials");
       } else if (!validEmail(email)) {
         return res.json("Invalid Email");
+      } else if (!validPass(password)) {
+        return res.json("Invalid Password. Password length should be longer than 5 characters");
       }
     } else if (req.path === "/login") {
       if (![email, password].every(Boolean)) {
         return res.json("Missing Credentials");
       } else if (!validEmail(email)) {
         return res.json("Invalid Email");
+      } else if (!validPass(password)) {
+        return res.json("Invalid Password. Password length should be longer than 5 characters");
       }
     }
   
